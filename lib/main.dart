887efe5d21413fce69c9c8efb1ea9c5959e1e2fd
vophong1612 <vophong1612@gmail.com>
2023-12-05 13:33:51 +0700

    imp. prevent rotate screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_movies_reviewer/presentation/app_root.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const AppRoot());
 }
